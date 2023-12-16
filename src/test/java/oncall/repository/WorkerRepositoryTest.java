68c4d140254dc9c4350af48104b217230f9af66c
JiHyeonL <lejinhy@gmail.com>
2023-12-16 17:58:14 +0900

    chore: 이전에 커밋 안되었던 것들 추가

---
+++ src/test/java/oncall/repository/WorkerRepositoryTest.java
@@ -0,0 +1,20 @@
+package oncall.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkerRepositoryTest {
+
+    @Test
+    void addAllWeekdayWorker() {
+    }
+
+    @Test
+    void addAllWeekendWorker() {
+    }
+
+    @Test
+    void calculateEmergencyWorkerAssignment() {
+    }
+}
