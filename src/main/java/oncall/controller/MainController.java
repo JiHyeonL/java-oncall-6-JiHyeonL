66dc852f34bdc5d3592996b1e061a1ded23b98a6
JiHyeonL <lejinhy@gmail.com>
2023-12-16 16:29:48 +0900

    feat: 배정 결과 계산, 출력 완료

--- src/main/java/oncall/controller/MainController.java
+++ src/main/java/oncall/controller/MainController.java
@@ -19,6 +19,8 @@ public class MainController {
         setEmergencyWeekdayWorkerWithValidation();
         setEmergencyWeekendDateWithValidation();
 
+        emergencyService.calculateEmergencyWorkerAssignment();
+        OutputView.writeWorkerResult(emergencyService.getAssignedWorker());
     }
 
 
