7af2e47b6539c6b2604fd301fb2d25b5ae2ee0cf
JiHyeonL <lejinhy@gmail.com>
2023-12-16 17:56:07 +0900

    refactor: dto활용해 배정 정보를 전달하는 방식으로 수정

--- src/main/java/oncall/repository/WorkerRepository.java
+++ src/main/java/oncall/repository/WorkerRepository.java
@@ -1,5 +1,7 @@
 package oncall.repository;
+import oncall.AssignmentFormat;
 import oncall.domain.Holiday;
+import oncall.domain.Month;
 import oncall.domain.Worker;
 
 import java.util.*;
@@ -7,20 +9,19 @@ import java.util.*;
 public class WorkerRepository {
     private final List<String> ALL_WEEKDAYS = List.of("월", "화", "수", "목", "금", "토", "일");
     private final List<String> WEEKDAY = List.of("월", "화", "수", "목", "금");
-    private final List<String> WEEKEND  = List.of("토", "일");
     private final String WEEKDAY_FORMAT = "평일";
     private final String WEEKEND_FORMAT = "휴일";
-    private final String FORMAT = "%d월 %d일 %s %s";
+    private final String WEEKDAY_HOLIDAY = "(휴일)";
 
     private List<Worker> weekday = new ArrayList<>();
     private List<Worker> weekend = new ArrayList<>();
     private int weekdayIndex = 0;
     private int weekendIndex = 0;
 
+    private List<Worker> result = new ArrayList<>();
     private int month = 0;
     private int dateOfWeekIndex = 0;
     private int date = 0;
-    private int endDate = 0;
 
     public void addAllWeekdayWorker(List<String> workers) {
         validate(workers);
@@ -45,54 +46,56 @@ public class WorkerRepository {
         }
     }
 
-    public List<String> calculateEmergencyWorkerAssignment(int month, String dateOfWeek, int endDate) {
-        this.month = month;
+    public List<AssignmentFormat> calculateEmergencyWorkerAssignment(Month month, String dateOfWeek) {
+        this.month = month.getMonth();
         this.dateOfWeekIndex = ALL_WEEKDAYS.indexOf(dateOfWeek);
         this.date = 1;
 
-        List<Worker> result = new ArrayList<>();
-        List<String> resultFormat = new ArrayList<>();
-        while (this.date <= endDate) {
-            if (weekdayIndex == weekday.size() || weekendIndex == weekend.size()) {
-                break;
-            }
-            if (isWeekday(dateOfWeekIndex, month, date)) {
-                Worker worker = weekday.get(weekdayIndex);
-
-                if (!result.isEmpty()) {
-                    if (result.get(result.size() - 1).isSameWorker(worker)) {
-                        Worker nowWorker = weekday.get(weekdayIndex);
-                        Worker nextWorker = weekday.get(weekdayIndex + 1);  // 인덱스 넘을 경우 확인
-                        weekday.set(weekdayIndex, nextWorker);
-                        weekday.set(weekdayIndex + 1, nowWorker);
-                    }
-                }
-                result.add(weekday.get(weekdayIndex));
-                resultFormat.add(String.format(FORMAT, this.month, this.date, ALL_WEEKDAYS.get(this.dateOfWeekIndex), weekday.get(weekdayIndex).getName()));
-
+        List<AssignmentFormat> resultFormat = new ArrayList<>();
+        while (this.date <= month.getLastDate()) {
+            if (isWeekday(dateOfWeekIndex, this.month, date)) {
+                resultFormat.add(addEmergencyWeekdayWorker());
                 setNextDayAndDateOfWeek(WEEKDAY_FORMAT);
                 continue;
             }
-            Worker worker = weekend.get(weekendIndex);
-
-            if (!result.isEmpty()) {
-                if (result.get(result.size() - 1).isSameWorker(worker)) {
-                    Worker nowWorker = weekend.get(weekendIndex);
-                    Worker nextWorker = weekend.get(weekendIndex + 1);  // 인덱스 넘을 경우 확인
-                    weekend.set(weekendIndex, nextWorker);
-                    weekend.set(weekendIndex + 1, nowWorker);
-                }
-            }
-            String dayOfWeek = ALL_WEEKDAYS.get(this.dateOfWeekIndex);
-            if (Holiday.isInHoliday(this.month, this.date) && WEEKDAY.contains(ALL_WEEKDAYS.get(dateOfWeekIndex))) {
-                dayOfWeek = dayOfWeek+"(휴일)";
-            }
-            result.add(weekend.get(weekendIndex));
-            resultFormat.add(String.format(FORMAT, this.month, this.date, dayOfWeek, weekend.get(weekendIndex).getName()));
+            resultFormat.add(addEmergencyWeekendWorker());
             setNextDayAndDateOfWeek(WEEKEND_FORMAT);
         }
 
         return resultFormat;
+    }
+
+    private AssignmentFormat addEmergencyWeekdayWorker() {
+        Worker worker = weekday.get(weekdayIndex);
+
+        if (!result.isEmpty()) {
+            if (result.get(result.size() - 1).isSameWorker(worker)) {
+                Worker nowWorker = weekday.get(weekdayIndex);
+                Worker nextWorker = weekday.get(weekdayIndex + 1);  // 인덱스 넘을 경우 확인
+                weekday.set(weekdayIndex, nextWorker);
+                weekday.set(weekdayIndex + 1, nowWorker);
+            }
+        }
+        result.add(weekday.get(weekdayIndex));
+        return new AssignmentFormat(this.month, this.date, ALL_WEEKDAYS.get(this.dateOfWeekIndex), weekday.get(weekdayIndex).getName());
+    }
+
+    private AssignmentFormat addEmergencyWeekendWorker() {
+        Worker worker = weekend.get(weekendIndex);
+        if (!result.isEmpty()) {
+            if (result.get(result.size() - 1).isSameWorker(worker)) {
+                Worker nowWorker = weekend.get(weekendIndex);
+                Worker nextWorker = weekend.get(weekendIndex + 1);  // 인덱스 넘을 경우 확인
+                weekend.set(weekendIndex, nextWorker);
+                weekend.set(weekendIndex + 1, nowWorker);
+            }
+        }
+        String dayOfWeek = ALL_WEEKDAYS.get(this.dateOfWeekIndex);
+        if (Holiday.isInHoliday(this.month, this.date) && WEEKDAY.contains(ALL_WEEKDAYS.get(dateOfWeekIndex))) {
+            dayOfWeek = dayOfWeek + WEEKDAY_HOLIDAY;
+        }
+        result.add(weekend.get(weekendIndex));
+        return new AssignmentFormat(this.month, this.date, dayOfWeek, weekend.get(weekendIndex).getName());
     }
 
 
--- src/main/java/oncall/service/EmergencyService.java
+++ src/main/java/oncall/service/EmergencyService.java
@@ -1,10 +1,12 @@
 package oncall.service;
 
+import oncall.AssignmentFormat;
+import oncall.domain.Month;
 import oncall.domain.Worker;
 import oncall.repository.WorkerRepository;
 
 import java.time.LocalDate;
-import java.time.Month;
+
 import java.time.YearMonth;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,16 +14,15 @@ import java.util.List;
 public class EmergencyService {
     private static final String LINE_SEPARATOR = System.lineSeparator();
     private final List<String> Weekdays = List.of("월", "화", "수", "목", "금", "토", "일");
-    private final List<Integer> MONTH = List.of(1,2,3,4,5,6,7,8,9,10,11,12);
+
     private final int MIN_WORKER_SIZE = 5;
     private final int MAX_WORKER_SIZE = 35;
 
     private final WorkerRepository workerRepository;
-    private List<String> assignedWorker;
+    private List<AssignmentFormat> assignedWorker;
 
-    private int month = 0;
+    private Month month;
     private String dateOfWeek = "";
-    private int endDay = 0;
 
 
     public EmergencyService(WorkerRepository workerRepository) {
@@ -29,25 +30,10 @@ public class EmergencyService {
     }
 
     public void setEmergencyDate(String month, String dateOfWeek) {
-        validateMonth(parseToInt(month));
         validateWeekdays(dateOfWeek);
-        this.month = parseToInt(month);
+
+        this.month = Month.getMonthFormat(parseToInt(month));
         this.dateOfWeek = dateOfWeek;
-
-        if (this.month == 2) {
-            this.endDay = 28;
-            return;
-        }
-
-        LocalDate localDate = LocalDate.of(2023, 5, 1);
-        int lastDayOfMonth = localDate.lengthOfMonth();
-        this.endDay = lastDayOfMonth;
-    }
-
-    public void validateMonth(int month) {
-        if (!MONTH.contains(month)) {
-            throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        }
     }
 
     public void validateWeekdays(String dateOfWeek) {
@@ -74,11 +60,16 @@ public class EmergencyService {
     }
 
     public void calculateEmergencyWorkerAssignment() {
-        this.assignedWorker = workerRepository.calculateEmergencyWorkerAssignment(month, dateOfWeek, endDay);
+        this.assignedWorker = workerRepository.calculateEmergencyWorkerAssignment(month, dateOfWeek);
     }
 
     public String getAssignedWorker() {
-        return String.join(LINE_SEPARATOR, assignedWorker);
+        List<String> assignmentResult = new ArrayList<>();
+
+        for (AssignmentFormat assignmentWorker : assignedWorker) {
+            assignmentResult.add(assignmentWorker.getAssignmentFormat());
+        }
+        return String.join(LINE_SEPARATOR, assignmentResult);
     }
 
     private static int parseToInt(String input) {
