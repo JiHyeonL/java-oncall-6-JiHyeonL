db712adfe7717c3a41cffbfdd3021b3516bc259d
JiHyeonL <lejinhy@gmail.com>
2023-12-16 16:30:52 +0900

    feat: 이름 비교 메소드 추가

--- src/main/java/oncall/domain/Worker.java
+++ src/main/java/oncall/domain/Worker.java
@@ -1,11 +1,25 @@
 package oncall.domain;
 
+import java.time.LocalDate;
+
 public class Worker {
+    private final String FORMAT = "%d월 %d일 %s %s";
     private final String name;
 
     public Worker(String name) {
         validate(name);
         this.name = name;
+    }
+
+    public boolean isSameWorker(Worker worker) {
+        if (this.name.equals(worker.name)) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
     }
 
     private void validate(String name) {
